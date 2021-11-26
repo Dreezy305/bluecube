ade77b9ccc4aafc287e31fa5e17d809dbee211f8
Idris Bankole <bankoleidris@gmail.com>
2021-11-27 00:39:09 +0100

    top bar

--- src/components/topBar.js
+++ src/components/topBar.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Search from "../assets/search.svg";
+import Bell from "../assets/bell.svg";
+import Avatar from "../assets/avatar_two.svg";
 
 function TopBar() {
   return (
-    <div className="d-flex flex-row justify-content-between w-100">
+    <div className="d-flex flex-row justify-content-around align-items-baseline w-100">
       <div className="topbar d-flex flex-row">
         <input
           type="text"
@@ -18,7 +20,11 @@ function TopBar() {
           Search
         </button>
       </div>
-      <div>buttons</div>
+      <div className="d-flex flex-row justify-content-around align-items-center">
+        <img src={Bell} className="img-fluid me-3" alt="notification_bell" />
+        <img src={Avatar} className="img-fluid ms-5" alt="avatar" />
+        <p className="mt-3 ms-5">Abigail</p>
+      </div>
     </div>
   );
 }
