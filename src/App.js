175143e4f360bb864aa761c7806503f0c157c0f7
Idris Bankole <bankoleidris@gmail.com>
2021-11-26 21:44:26 +0100

    loading assets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "./styles/globals.scss";
+import LeftSideBar from "./components/leftSideBar";
 
 function App() {
   return (
     <div className="d-flex flex-row justify-content-between">
-      <h1>Blue cube</h1>
+      <LeftSideBar />
     </div>
   );
 }
