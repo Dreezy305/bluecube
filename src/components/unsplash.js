3dad1fc9e0ac447b7c22f34190043d91940848c3
Idris Bankole <bankoleidris@gmail.com>
2021-11-27 23:26:41 +0100

    heart && thumbdown

--- src/components/unsplash.js
+++ src/components/unsplash.js
@@ -45,10 +45,25 @@ export default function Unsplash() {
                     <span className="">{image.user.last_name}</span>,
                     <span className="">{image.user.total_photos}</span>
                   </h2>
-                  <p>
+                  <p className="d-flex flex-row" id="p">
                     <span>
-                      <img src={Map} className="" alt="location" />
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="10"
+                        height="10"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        class="feather feather-map-pin"
+                      >
+                        <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
+                        <circle cx="12" cy="10" r="3"></circle>
+                      </svg>
                     </span>
+                    <span>{image.user.location}</span>
                   </p>
                 </div>
               </div>
